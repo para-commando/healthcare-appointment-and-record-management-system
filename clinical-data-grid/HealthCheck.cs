6db0c867e8e3d377ab24bab6f28e98a92de1e7dc
para-commando <anirudh.nayak.172k@gmail.com>
2024-12-04 00:05:35 +0530

    swagger integration using custom extension functions, health-check api, added loggers

---
+++ clinical-data-grid/HealthCheck.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using database.dbContext;
+namespace clinical_data_grid
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        private readonly postgresHealthCareDbContext _dbContext;
+        private readonly ILogger<HealthCheckController> _logger;
+
+        public HealthCheckController(postgresHealthCareDbContext dbContext, ILogger<HealthCheckController> logger)
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("health-check")]
+        public async Task<IActionResult> GetHealthStatus()
+        {
+            try
+            {
+                _logger.LogInformation("Initiating health check for db connection at {Time}", DateTime.UtcNow);
+                var dbConnectionStatus = await _dbContext.Database.CanConnectAsync();
+                _logger.LogInformation("Initiating health check for cache connection at {Time}", DateTime.UtcNow);
+                // Simulated checks for other services
+                var cacheServiceStatus = true;
+                _logger.LogInformation("Initiating health check for external api connection at {Time}", DateTime.UtcNow);
+                var externalApiStatus = true;
+                // Log the health check (optional)
+                _logger.LogInformation("Health check completed successfully at {Time}", DateTime.UtcNow);
+
+                // Create health report
+                var healthReport = new
+                {
+                    RepositoryStatus = dbConnectionStatus ? "Healthy" : "Unhealthy",
+                    CacheService = cacheServiceStatus ? "Healthy" : "Unhealthy",
+                    ExternalApi = externalApiStatus ? "Healthy" : "Unhealthy",
+                    Timestamp = DateTime.UtcNow
+                };
+
+                return Ok(healthReport);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred during the health check.");
+                return StatusCode(500, new { Message = "An error occurred while performing the health check." });
+            }
+        }
+    }
+}
--- clinical-data-grid/Program.cs
+++ clinical-data-grid/Program.cs
@@ -1,11 +1,36 @@
+using clinical_data_grid.extensions;
 using database.dbContext;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+// Register Swagger generator with proper grouping logic
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
+var connectionString =
+    builder.Configuration.GetConnectionString("postgresHealthCareDB")
+        ?? throw new InvalidOperationException("Connection string"
+        + "'postgresHealthCareDB' not found.");
 builder.Services.AddDbContext<postgresHealthCareDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("postgresHealthCareDB")));
-var app = builder.Build();
+    options.UseNpgsql(connectionString));
+// configuring logger services
+builder.Services.AddLogging(configure =>
+{
+    // Clears all existing logging providers
+    configure.ClearProviders();
 
-app.MapGet("/", () => "Hello World!");
+    // Adds the Console logging provider
+    configure.AddConsole();
+
+    // Adds the Debug logging provider
+    configure.AddDebug();
+
+    // Sets the minimum log level to Debug
+    configure.SetMinimumLevel(LogLevel.Debug);
+});
+builder.AddSwaggerGenCustExt();
+var app = builder.Build();
+app.MapControllers();
+app.UseSwaggerCustExt();
+
 
 app.Run();
