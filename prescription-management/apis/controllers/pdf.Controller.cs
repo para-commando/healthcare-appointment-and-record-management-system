5e981fcfcd56f9140490d8ce5b1a228000f84368
para-commando <anirudh.nayak.172k@gmail.com>
2024-12-14 10:49:02 +0530

    adding comments and redundant files cleanup

--- prescription-management/apis/controllers/pdf.Controller.cs
+++ prescription-management/apis/controllers/pdf.Controller.cs
@@ -6,18 +6,18 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
-[AllowAnonymous]
 [Route("[controller]")]
 public class PdfController : ControllerBase
 {
 
-    [HttpPost("generate")]
+    [HttpPost("generate-prescription-pdf")]
     public IActionResult GeneratePrescriptionPdf([FromBody] PrescriptionContent prescriptionContent)
     {
         string htmlContent = HtmlGenerator.GeneratePrescriptionHtml(prescriptionContent);
-
         Console.WriteLine(prescriptionContent);
+        // MemoryStream is a class in .NET that provides a stream of data stored in memory (RAM) rather than a file or another medium
         using var stream = new MemoryStream();
+        // HtmlConverter.ConvertToPdf() method writes the generated PDF into a stream. Using a MemoryStream avoids the need to create a physical file on disk, improving performance and reducing IO overhead.
         HtmlConverter.ConvertToPdf(htmlContent, stream);
         return File(stream.ToArray(), "application/pdf", "GeneratedDocument.pdf");
     }
