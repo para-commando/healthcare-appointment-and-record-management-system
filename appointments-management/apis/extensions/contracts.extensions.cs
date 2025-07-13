5e0494ef19bfa0b01ce7eeb9aee4f607c1d027eb
para-commando <anirudh.nayak.172k@gmail.com>
2025-07-13 13:20:35 +0530

    formatting

--- appointments-management/apis/extensions/contracts.extensions.cs
+++ appointments-management/apis/extensions/contracts.extensions.cs
@@ -1,23 +1,22 @@
-
 using appointment_details.apis.contracts;
 using appointment_details.database.models;
-
 
 namespace appointments_management.apis.extensions;
 
 public static class ContractsExtensions
 {
-  public static AppointmentDetails ReturnAnEntityObject(this AppointmentDetailsContract appointmentDetailsContract)
-  {
-    return new AppointmentDetails
+    public static AppointmentDetails ReturnAnEntityObject(
+        this AppointmentDetailsContract appointmentDetailsContract
+    )
     {
-      PatientId = appointmentDetailsContract.PatientId,
-      DoctorId = appointmentDetailsContract.DoctorId,
-      AppointmentDate = appointmentDetailsContract.AppointmentDate,
-      AppointmentTimeOfDay = appointmentDetailsContract.AppointmentTimeOfDay,
-      AppointmentBookedOn = appointmentDetailsContract.AppointmentBookedOn,
-      AppointmentStatus = appointmentDetailsContract.AppointmentStatus,
-    };
-  }
-
+        return new AppointmentDetails
+        {
+            PatientId = appointmentDetailsContract.PatientId,
+            DoctorId = appointmentDetailsContract.DoctorId,
+            AppointmentDate = appointmentDetailsContract.AppointmentDate,
+            AppointmentTimeOfDay = appointmentDetailsContract.AppointmentTimeOfDay,
+            AppointmentBookedOn = appointmentDetailsContract.AppointmentBookedOn,
+            AppointmentStatus = appointmentDetailsContract.AppointmentStatus,
+        };
+    }
 }
--- appointments-management/apis/extensions/swagger.extensions.cs
+++ appointments-management/apis/extensions/swagger.extensions.cs
@@ -1,74 +1,85 @@
-
 namespace appointment_details.apis.extensions;
 
 public static class SwaggerExtensions
 {
-  public static WebApplicationBuilder AddSwaggerGenCustExt(this WebApplicationBuilder builder)
-  {
-    builder.Services.AddSwaggerGen(options =>
-  {
-    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    public static WebApplicationBuilder AddSwaggerGenCustExt(this WebApplicationBuilder builder)
     {
-      Title = "My API",
-      Version = "v1",
-      Description = "API for managing resources.",
-      Contact = new Microsoft.OpenApi.Models.OpenApiContact
-      {
-        Name = "Ghatak Commando",
-      }
-    });
-
-    options.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo
-    {
-      Title = "My API",
-      Version = "v2",
-      Description = "API for managing resources.",
-      Contact = new Microsoft.OpenApi.Models.OpenApiContact
-      {
-        Name = "Para Commando",
-      }
-    });
-
-    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+        builder.Services.AddSwaggerGen(options =>
         {
-            Name = "Authorization",
-            Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
-            Scheme = "bearer",
-            BearerFormat = "JWT",
-            In = Microsoft.OpenApi.Models.ParameterLocation.Header,
-            Description = "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6...\""
-        });
+            options.SwaggerDoc(
+                "v1",
+                new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "My API",
+                    Version = "v1",
+                    Description = "API for managing resources.",
+                    Contact = new Microsoft.OpenApi.Models.OpenApiContact
+                    {
+                        Name = "Ghatak Commando",
+                    },
+                }
+            );
 
-        options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
-        {
-            {
+            options.SwaggerDoc(
+                "v2",
+                new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "My API",
+                    Version = "v2",
+                    Description = "API for managing resources.",
+                    Contact = new Microsoft.OpenApi.Models.OpenApiContact
+                    {
+                        Name = "Para Commando",
+                    },
+                }
+            );
+
+            options.AddSecurityDefinition(
+                "Bearer",
                 new Microsoft.OpenApi.Models.OpenApiSecurityScheme
                 {
-                    Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                    Name = "Authorization",
+                    Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+                    Description =
+                        "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6...\"",
+                }
+            );
+
+            options.AddSecurityRequirement(
+                new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+                {
                     {
-                        Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                    }
-                },
-                new string[] {}
-            }
+                        new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                        {
+                            Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                            {
+                                Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                                Id = "Bearer",
+                            },
+                        },
+                        new string[] { }
+                    },
+                }
+            );
         });
-        
-  });
-    return builder;
-  }
-  public static WebApplication UseSwaggerCustExt(this WebApplication app)
-  {
-    app.UseSwagger(options =>
+        return builder;
+    }
+
+    public static WebApplication UseSwaggerCustExt(this WebApplication app)
     {
-      options.RouteTemplate = "swagger/{documentName}/swagger.json";
-    });
-    app.UseSwaggerUI(c =>
-       {
-         c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
-         c.SwaggerEndpoint("/swagger/v2/swagger.json", "My API v2");
-         c.RoutePrefix = string.Empty; // Swagger at root
-       });
-    return app;
-  }
+        app.UseSwagger(options =>
+        {
+            options.RouteTemplate = "swagger/{documentName}/swagger.json";
+        });
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+            c.SwaggerEndpoint("/swagger/v2/swagger.json", "My API v2");
+            c.RoutePrefix = string.Empty; // Swagger at root
+        });
+        return app;
+    }
 }
